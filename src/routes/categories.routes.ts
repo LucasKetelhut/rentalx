e73463ddfd107a6c1240a053dc08d97e42e1fb09
Lucas Ketelhut <lucas-kmk@hotmail.com>
2021-11-16 22:18:12 -0300

    Trabalhando com upload de categories atraves do multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp"
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -12,4 +19,8 @@ categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 })
 
-export { categoriesRoutes };
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handler(request, response);
+});
+
+export { categoriesRoutes }
